094ec7df6af825dc53a43084faebf3c93bedf489
Rary Gonçalves <rarygc@gmail.com>
2021-02-26 18:32:21 -0300

    Add base classes for API resources

--- api/__init__.py
+++ api/__init__.py
@@ -8,4 +8,16 @@ class Index(Resource):
     def get(self):
         return {'greeting': 'Hello, DevGrid!'}
 
+class WeatherCityList(Resource):
+    def get(self):
+
+        cities = [{'name': 'Floripa'}, {'name': 'Natal'}]
+        return {'cities': cities}
+
+class WeatherCity(Resource):
+    def get(self, city_name):
+        return {'city': city_name}
+
 api.add_resource(Index, '/')
+api.add_resource(WeatherCityList, '/weather')
+api.add_resource(WeatherCity, '/weather/<string:city_name>')
