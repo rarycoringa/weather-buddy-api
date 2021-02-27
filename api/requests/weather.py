0e309800b2428943e2e59547efa4b46227ab597c
Rary Gonçalves <rarygc@gmail.com>
2021-02-26 22:26:47 -0300

    Add Openweathermap API requests with my credentials

---
+++ api/requests/weather.py
@@ -0,0 +1,27 @@
+import requests
+
+def request_openweathermap(city_name):
+    BASE_URL = 'api.openweathermap.org/data/2.5/weather'
+    API_KEY = '3f62164f8ce1f8cb5ae8e2d02918babb'
+
+    payload = {
+        'q': city_name,
+        'appid': API_KEY,
+        'units': 'metric',
+    }
+
+    try:
+        json = requests.get(f'https://{BASE_URL}', params=payload).json()
+    except:
+        return {'message': 'Connection to the openweathermap service failed'}, 503
+
+    try:
+        response = {
+            'city': json['name'],
+            'temp': json['main']['temp'],
+            'weather': json['weather'][0]['main']
+        }
+    except:
+        return {'message': 'City not found'}, 404
+
+    return response
