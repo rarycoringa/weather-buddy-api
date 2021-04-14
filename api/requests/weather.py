efb02114e7473a4a53d48bdeb09f5451715fb013
Rary Gonçalves <rarygc@gmail.com>
2021-04-14 16:55:19 -0300

    Update weather request with new data modeling
    The 'weather' object is returned as a Weather model instance

--- api/requests/weather.py
+++ api/requests/weather.py
@@ -1,4 +1,5 @@
 import os, requests
+from api.models.weather import PhysicalQuantity, City, Wind, Weather
 
 def request_openweathermap(city_name):
     '''
@@ -22,11 +23,45 @@ def request_openweathermap(city_name):
         return {'message': 'Connection to the OpenWeatherMap API service failed'}, 503
 
     try:
-        weather = {
-            'city': response['name'],
-            'temp': response['main']['temp'],
-            'weather': response['weather'][0]['description'].title()
-        }
+        weather = Weather({
+            'city': City({
+                'name': response['name'].title(),
+                'country': response['sys']['country'].upper(),
+                'coordinates': [response['coord']['lat'], response['coord']['lon']]
+            }),
+            'description': response['weather'][0]['main'].title(),
+            'long_description': response['weather'][0]['description'].title(),
+            'temperature': PhysicalQuantity({
+                'value': float(response['main']['temp']),
+                'unit': 'celsius degree'
+            }),
+            'feels_like': PhysicalQuantity({
+                'value': float(response['main']['feels_like']),
+                'unit': 'celsius degree'
+            }) if response['main']['feels_like'] != response['main']['temp'] else None,
+            'max_temperature': PhysicalQuantity({
+                'value': float(response['main']['temp_max']),
+                'unit': 'celsius degree'
+            }) if response['main']['temp_max'] != response['main']['temp'] else None,
+            'min_temperature': PhysicalQuantity({
+                'value': float(response['main']['temp_min']),
+                'unit': 'celsius degree'
+            }) if response['main']['temp_min'] != response['main']['temp'] else None,
+            'wind': Wind({
+                'speed': PhysicalQuantity({
+                    'value': float(response['wind']['speed']),
+                    'unit': 'meters per second'
+                }),
+                'degree': PhysicalQuantity({
+                    'value': float(response['wind']['deg']),
+                    'unit': 'degrees'
+                })
+            }),
+            'visibility': PhysicalQuantity({
+                'value': float(response['visibility']),
+                'unit': 'meters'
+            })
+        })
     except:
         return {'message': 'Weather not found'}, 404
 
