9852f77c2fde47b5de634e240f4a4bd6f4da462f
Rary Gonçalves <rarygc@gmail.com>
2021-04-14 11:23:31 -0300

    Add first version of the models with schematics

---
+++ api/models/weather.py
@@ -0,0 +1,32 @@
+from schematics.models import Model
+from schematics.types import BaseType, FloatType, GeoPointType, ModelType, StringType
+
+class City(Model):
+    name = StringType(required=True)
+    country = StringType()
+    coordinates = GeoPointType()
+
+    class Options:
+        serialize_when_none = False
+
+class Wind(Model):
+    speed = FloatType(required=True)
+    degree = FloatType(required=True)
+    gust = FloatType(required=True)
+
+    class Options:
+        serialize_when_none = False
+
+class Weather(Model):
+    city = ModelType(City, required=True)
+    description = StringType(required=True)
+    long_description = StringType(required=True)
+    temperature = FloatType(required=True)
+    feels_like = FloatType()
+    max_temperature = FloatType()
+    min_temperature = FloatType()
+    wind = ModelType(Wind, required=True)
+    visibility = FloatType()
+
+    class Options:
+        serialize_when_none = False
