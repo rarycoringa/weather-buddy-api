a98a33e0e4af839841397e604e5311f3877da27f
Rary Gonçalves <rarygc@gmail.com>
2021-04-22 08:40:13 -0300

    Creates a OpenWeatherMap instance and configure

--- api/__init__.py
+++ api/__init__.py
@@ -2,15 +2,35 @@ from flask import Flask
 from flask_restful import Api
 from flask_caching import Cache
 
+from api.utils.openweathermap import OpenWeatherMap
+
+import os
+
 # Create and configure Flask App instance
 app = Flask(__name__)
 
 # Create and configure Flask Cache instance
-cache = Cache(app, config={'CACHE_TYPE': 'flask_caching.backends.SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 300})
+cache = Cache(app,
+    config={
+        'CACHE_TYPE': 'flask_caching.backends.SimpleCache',
+        'CACHE_DEFAULT_TIMEOUT': 300
+    }
+)
+
 cache.init_app(app)
 
 # Create a Flask RESTful Api instance
 api = Api(app)
+
+# Create a OpenWeatherMap Api instance
+openweathermap = OpenWeatherMap(
+    settings = {
+        'API_KEY': os.environ.get('OPENWEATHERMAP_TOKEN'),
+        'BASE_URL': 'api.openweathermap.org',
+        'VERSION': '2.5',
+        'SERVICE': 'weather'
+    }
+)
 
 # Import Api endpoints
 from api.resources.index import IndexView
