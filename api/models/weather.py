d56947c6a6cf064601818f5e6e4961e08041b597
Rary Gonçalves <rarygc@gmail.com>
2021-04-23 12:51:34 -0300

    Add new Temperature model with serializable methods of the unit conversion

--- api/models/weather.py
+++ api/models/weather.py
@@ -1,9 +1,49 @@
 from schematics.models import Model
 from schematics.types import FloatType, GeoPointType, ModelType, StringType
+from schematics.types.serializable import serializable
+from enum import Enum
 
 class PhysicalQuantity(Model):
     value = FloatType(required=True)
     unit = StringType(required=True)
+
+class Temperature(Model):
+    value = FloatType(required=True)
+
+    class TemperatureMetrics(Enum):
+        CELSIUS = 1
+        FAHRENHEIT = 2
+        KELVIN = 3
+
+    @serializable(type=FloatType, serialized_name='value')
+    def get_temperature(self, *args, **kwargs):
+        if hasattr(self, 'context'):
+            temperature = self.context.get('temperature', 'kelvin')
+
+            if temperature.upper() == self.TemperatureMetrics.CELSIUS.name:
+                value = round(self.value - 273.0, 2) # Converts Kelvin temperature to Celsius
+            elif temperature.upper() == self.TemperatureMetrics.FAHRENHEIT.name:
+                value = round(1.8*(self.value-273.0)+32.0, 2) # Converts Kelvin temperature to Fahrenheit
+            else:
+                value = round(self.value, 2) # Returns Kelvin temperature
+        else:
+            value = round(self.value, 2) # Returns Kelvin temperature
+        return value
+
+    @serializable(type=StringType, serialized_name='unit')
+    def get_unit(self, *args, **kwargs):
+        if hasattr(self, 'context'):
+            unit = self.context.get('temperature', 'kelvin')
+
+            if unit.upper() == self.TemperatureMetrics.CELSIUS.name:
+                unit = self.TemperatureMetrics.CELSIUS
+            elif unit.upper() == self.TemperatureMetrics.FAHRENHEIT.name:
+                unit = self.TemperatureMetrics.FAHRENHEIT
+            else:
+                unit = self.TemperatureMetrics.KELVIN
+        else:
+            unit = self.TemperatureMetrics.KELVIN
+        return unit.name.lower()
 
 
 class City(Model):
@@ -27,10 +67,10 @@ class Weather(Model):
     city = ModelType(City, required=True)
     description = StringType(required=True)
     long_description = StringType(required=True)
-    temperature = ModelType(PhysicalQuantity, required=True)
-    feels_like = ModelType(PhysicalQuantity)
-    max_temperature = ModelType(PhysicalQuantity)
-    min_temperature = ModelType(PhysicalQuantity)
+    temperature = ModelType(Temperature, required=True)
+    feels_like = ModelType(Temperature)
+    max_temperature = ModelType(Temperature)
+    min_temperature = ModelType(Temperature)
     wind = ModelType(Wind, required=True)
     visibility = ModelType(PhysicalQuantity)
 
